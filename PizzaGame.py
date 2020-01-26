1478d814064d1e94ff5fbb9d9787595374db1957
Kudinov <kudinovmiha@gmail.com>
2020-01-26 20:57:58 +0300

    pizza and pan

--- PizzaGame.py
+++ PizzaGame.py
@@ -10,28 +10,36 @@ class Pizza(games.Sprite):
         if self.bottom > games.screen.height or self.top < 0:
             self.dy = - self.dy
 
-class New():
-    def create_pizza():
-        pizza_image = games.load_image("pizza.bmp")
-        the_pizza = Pizza(image = pizza_image,
-                            x = random.randint(50, 450),
-                            y = random.randint(50, 600),
-                            dx =2,
-                            dy =2)
-        games.screen.add(the_pizza)
+class Pan(games.Sprite):
+    """ Сковорода """
+    def update(self):
+        """ Перемещение в позицию курсора"""
+        self.x = games.mouse.x
+        self.y = games.mouse.y
+
 
 def main():
     wall_image = games.load_image("wall.jpg", transparent=False)
     games.screen.background = wall_image
-    """pizza_image = games.load_image("pizza.bmp")
+
+    pizza_image = games.load_image("pizza.bmp")
     the_pizza = Pizza(image = pizza_image,
                          x = games.screen.width/2,
                          y = games.screen.height/2,
                          dx =2,
                          dy =2)
-    games.screen.add(the_pizza)"""
-    for piz in range(5):
-        New.create_pizza()
+    games.screen.add(the_pizza)
+
+    pan_image = games.load_image("pan.bmp")
+    the_pan = Pan(image = pan_image,
+                  x = games.mouse.x,
+                  y = games.mouse.y)
+    games.screen.add(the_pan)
+
+    games.mouse.is_visible = False
+    games.screen.event_grab = False
+
+    
     games.screen.mainloop()
 main()
 """won_message = games.Message(value = "Victory!", 
@@ -57,3 +65,16 @@ games.screen.add(the_pizza)"""
 """score = games.Text(value = 1756521, size = 60, color = color.black, x = 550, y = 30)
 games.screen.add(score)
 """
+
+"""class New():
+    def create_pizza():
+        pizza_image = games.load_image("pizza.bmp")
+        the_pizza = Pizza(image = pizza_image,
+                            x = random.randint(50, 450),
+                            y = random.randint(50, 600),
+                            dx =2,
+                            dy =2)
+        games.screen.add(the_pizza)"""
+
+"""for piz in range(5):
+        New.create_pizza()"""
