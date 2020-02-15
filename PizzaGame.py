b6fab923190e0f71d8e47e99263258c87a8c04f5
Kudinov <kudinovmiha@gmail.com>
2020-02-15 18:33:00 +0300

    Create documentation

--- PizzaGame.py
+++ PizzaGame.py
@@ -3,9 +3,17 @@ import random
 games.init(screen_width = 640, screen_height = 480, fps = 50)
 
 class Pan(games.Sprite):
+    """
+    Сковорода, в которую игрок ловит пиццу.
+    """
+
     image = games.load_image("pan.bmp")
 
     def __init__(self):
+        """
+        Инициализирует объект Pan и создает объет Text для отображения счета.
+        """
+
         super(Pan, self).__init__(image = Pan.image,
                                   x = games.mouse.x,
                                   bottom = games.screen.height)      
@@ -14,6 +22,10 @@ class Pan(games.Sprite):
         games.screen.add(self.score)
 
     def update(self):
+        """
+        Передвигает объект по горизонтали в точку с абсциссой, как у указателя мыши.
+        """
+
         self.x = games.mouse.x
         if self.left < 0:
             self.left = 0
@@ -22,27 +34,52 @@ class Pan(games.Sprite):
         self.check_catch()
 
     def check_catch(self):
+        """
+        Проверяет, поймал ли игрок пиццу.
+        """
+
         for pizza in self.overlapping_sprites:
             self.score.value += 10
             self.score.right = games.screen.width - 10 
             pizza.handle_caught()
 
 class Pizza(games.Sprite):
+    """
+    Круги пиццы, падающие на землю.
+    """
+
     image = games.load_image("pizza.bmp")
     speed = 1
     def __init__(self, x, y = 90):
+        """
+        Инициализирует объет Pizza.
+        """
+
         super(Pizza, self).__init__(image = Pizza.image,
                                     x = x, y = y,
                                     dy = Pizza.speed)
 
     def update(self):
+        """
+        Проверяет, не коснулась ли нижняя кромка спрайта нижней границы экрана.
+        """
+
         if self.bottom > games.screen.height:
             self.end_game()
             self.destroy()
 
     def handle_caught(self):
-       self.destroy()
+        """
+        Разрушает объект, пойманный игроком.
+        """
+
+        self.destroy()
+
     def end_game(self):
+        """
+        Завершает игру.
+        """
+
         end_message = games.Message(value = "Game Over",
                                     size = 90,
                                     color = color.red,
@@ -53,10 +90,17 @@ class Pizza(games.Sprite):
         games.screen.add(end_message)
 
 class Chef(games.Sprite):
+    """
+    Повар, который двигается влево-вправо и разбрасывает пиццу.
+    """
+
     image = games.load_image("chef.bmp")
 
     def __init__(self, y = 55, speed = 2, odds_change = 200):
-        """ Initialize the Chef object. """
+        """
+        Инициализирует объект Chef.
+        """
+
         super(Chef, self).__init__(image = Chef.image,
                                    x = games.screen.width / 2,
                                    y = y,
@@ -65,6 +109,10 @@ class Chef(games.Sprite):
         self.time_til_drop = 0
 
     def update(self):
+        """
+        Определяет, надо ли сменить направление.
+        """
+
         if self.left < 0 or self.right > games.screen.width:
             self.dx = -self.dx
         elif random.randrange(self.odds_change) == 0:
@@ -72,15 +120,24 @@ class Chef(games.Sprite):
         self.check_drop()
 
     def check_drop(self):
+        """
+        Уменьшает интервал ожидания на единицу или сбрасывает очередную пиццу
+        и восстанавливает исходный интервал.
+        """
+
         if self.time_til_drop > 0:
             self.time_til_drop -= 1
         else:
             new_pizza = Pizza(x = self.x)
             games.screen.add(new_pizza)
-            # set buffer to approx 30% of pizza height, regardless of pizza speed   
+            # вне зависимости от скорости падения пиццы "зазор" между падающими кругами
+            # принимается равным 30% каждого из них по высоте
             self.time_til_drop = int(new_pizza.height * 1.3 / Pizza.speed) + 1
 
 def main():
+    """
+    Игровой процесс.
+    """
     wall_image = games.load_image("wall.jpg", transparent = False)
     games.screen.background = wall_image
 
